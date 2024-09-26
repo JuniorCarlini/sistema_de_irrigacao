df02c17317fe8b9f717df87a1ea443f35cb21f48
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-09-26 13:28:06 -0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from users.views import login_view
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('login/', login_view, name='login'),
     path('', include('dashboard.urls')),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
