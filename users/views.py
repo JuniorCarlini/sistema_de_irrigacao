2ded73a7ece42d941f15b53e023c93d1a00870d4
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-09-26 13:29:51 -0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,26 +1,18 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from .models import User
-from .forms import LoginForm
-
 
 def login_view(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            try:
-                # Busca o usuário no banco de dados
-                user = User.objects.get(name=username, password=password)
-                # Autenticação bem-sucedida
-                messages.success(request, f"Bem-vindo, {user.name}!")
-                return redirect('dashboard')  # Redireciona para a página inicial ou dashboard
-            except User.DoesNotExist:
-                messages.error(request, "Usuário ou senha incorretos.")
-        else:
-            messages.error(request, "Por favor, preencha o formulário corretamente.")
-    else:
-        form = LoginForm()
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
 
-    return render(request, 'users/html/login.html', {'form': form})
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirecionar para a página principal ou dashboard
+            return redirect('dashboard')  # Ajuste a URL conforme necessário
+        else:
+            messages.error(request, 'Usuário ou senha inválidos.')
+
+    return render(request, 'users/html/login.html')
