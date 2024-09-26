9229bcbba893a94d39b89a6d97293257a8d74165
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-09-26 13:29:39 -0300

    Delete forms.py

--- users/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Digite seu usuário'}))
-    password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Digite sua senha'}))
