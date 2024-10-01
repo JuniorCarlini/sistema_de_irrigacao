30898758fe82237d0bfde7df3c46f3ed8479a56f
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-10-01 13:26:23 -0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users.views import login_view , about_view
 from django.contrib.auth.views import LogoutView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('', include('dashboard.urls')),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
