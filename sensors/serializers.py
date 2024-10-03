ba0e42674fb30cda3775b8f84444a7b6acbd412f
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-10-02 21:39:58 -0300

    Update serializers.py

--- sensors/serializers.py
+++ sensors/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import SensorData
+from .models import DataCollection, SolenoidState
 
-class SensorDataSerializer(serializers.ModelSerializer):
+class SolenoidStateSerializer(serializers.ModelSerializer):
     class Meta:
-        model = SensorData
-        fields = '__all__'
+        model = SolenoidState
+        fields = ['is_open']
+
+class DataCollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataCollection
+        fields = ['temperature', 'air_humidity', 'soil_humidity']
