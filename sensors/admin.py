bc45456bb637f7676594ea143e9e2a79eebcb0c1
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-10-02 21:40:08 -0300

    Update admin.py

--- sensors/admin.py
+++ sensors/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import SensorData
+from .models import SolenoidState, DataCollection
 
-admin.site.register(SensorData)
+admin.site.register(SolenoidState)
+admin.site.register(DataCollection)
