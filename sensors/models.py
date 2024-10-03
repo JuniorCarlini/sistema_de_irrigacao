fc46795a73cfdcfeeac0ededa28c8a1ae7757a02
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-10-02 21:38:34 -0300

    Update models.py

--- sensors/models.py
+++ sensors/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
-class SensorData(models.Model):
+class SolenoidState(models.Model):
+    is_open = models.BooleanField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Solenoid is {'open' if self.is_open else 'closed'} at {self.timestamp}"
+
+class DataCollection(models.Model):
     temperature = models.FloatField()
     air_humidity = models.FloatField()
     soil_humidity = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Temp: {self.temperature}, Air Humidity: {self.air_humidity}, Soil Humidity: {self.soil_humidity}"
+        return f"Data: Temp={self.temperature}, Air Humidity={self.air_humidity}, Soil Humidity={self.soil_humidity}"
