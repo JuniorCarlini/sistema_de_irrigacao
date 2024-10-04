2f787d09593c9ca8a9c6733786433e9b7c99fd1b
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-10-04 01:16:31 -0300

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import dashboard_view
+from .views import dashboard_view , configuration_view
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),
+    path('configuration/', configuration_view, name='configuration'),
 ]
