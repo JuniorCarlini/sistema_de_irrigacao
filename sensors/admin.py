0c8a000fa240dccbbf1910acb8c22cf84a8d3013
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-10-09 14:06:39 -0300

    Update admin.py

--- sensors/admin.py
+++ sensors/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import SolenoidState, DataCollection, FlowRate, WaterUsage
+from .models import SolenoidState, DataCollection, FlowRate, WaterUsage, Configuracao
 
 admin.site.register(SolenoidState)
 admin.site.register(DataCollection)
 admin.site.register(FlowRate)
 admin.site.register(WaterUsage)
+admin.site.register(Configuracao)
