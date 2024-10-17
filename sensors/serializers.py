5eadb4726f497972024ee21fbab1a126e5f672f4
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-10-17 14:07:43 -0300

    Update serializers.py

--- sensors/serializers.py
+++ sensors/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import DataCollection, SolenoidState, StatusFertil, StoricFertil
+from .models import DataCollection, SolenoidState, StatusFertil, StoricFertil, TimeFerti
 
 class SolenoidStateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,12 +11,27 @@ class DataCollectionSerializer(serializers.ModelSerializer):
         model = DataCollection
         fields = ['temperature', 'air_humidity', 'soil_humidity']
 
-class StartFertilSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StatusFertil
-        fields = ['start_fertil']
-
 class StoricFertilSerializer(serializers.ModelSerializer):
     class Meta:
         model = StoricFertil
         fields = ['data_fertil_irrigacao']
+
+class CombinedSerializer(serializers.Serializer):
+    time_ferti_ms = serializers.IntegerField()
+    start_fertil = serializers.BooleanField()
+
+    def to_representation(self, instance):
+        # Obtém os dados de TimeFerti e StatusFertil
+        time_ferti = TimeFerti.objects.first()
+        status_fertil = StatusFertil.objects.first()
+
+        if time_ferti and status_fertil:
+            return {
+                'time_ferti_ms': time_ferti.time_ferti_ms,
+                'start_fertil': status_fertil.start_fertil,
+            }
+        else:
+            return {
+                'time_ferti_ms': None,
+                'start_fertil': None,
+            }
