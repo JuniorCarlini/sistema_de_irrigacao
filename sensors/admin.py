fa5258f5f60554eaadd2357c0cbea75d576ed04a
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-10-17 11:25:33 -0300

    Update admin.py

--- sensors/admin.py
+++ sensors/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import SolenoidState, DataCollection, FlowRate, WaterUsage, Configuracao
+from .models import SolenoidState, DataCollection, FlowRate, WaterUsage, Configuracao, StatusFertil , ConfigFertil, StoricFertil
 
 admin.site.register(SolenoidState)
 admin.site.register(DataCollection)
 admin.site.register(FlowRate)
 admin.site.register(WaterUsage)
 admin.site.register(Configuracao)
+admin.site.register(StatusFertil)
+admin.site.register(ConfigFertil)
+admin.site.register(StoricFertil)
