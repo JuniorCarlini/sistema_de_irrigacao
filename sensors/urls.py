a0173219e121e7b5a6954056935c2384715867c6
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-10-17 11:25:43 -0300

    Update urls.py

--- sensors/urls.py
+++ sensors/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import collect_environmental_data, collect_solenoid_state, temperature_chart, air_humidity_chart, soil_humidity_chart, water_usage_view
+from .views import collect_environmental_data, collect_solenoid_state, temperature_chart, air_humidity_chart, soil_humidity_chart, water_usage_view, fertil_usage_view, get_data_fertil_state, collect_historico_fertil
 
 urlpatterns = [
     path('collect-data/', collect_environmental_data, name='collect_environmental_data'),
@@ -8,4 +8,7 @@ urlpatterns = [
     path('air-humidity-chart/', air_humidity_chart, name='air_humidity_chart'),
     path('soil-humidity-chart/', soil_humidity_chart, name='soil_humidity_chart'),
     path('water-usage/', water_usage_view, name='water_usage_view'),
+    path('fertil-usage/', fertil_usage_view, name='fertil_usage_view'),
+    path('data-fertil_state/', get_data_fertil_state, name='get_data_fertil_state'),
+    path('historico-fertil/', collect_historico_fertil, name='collect_historico_fertil'),
 ]
