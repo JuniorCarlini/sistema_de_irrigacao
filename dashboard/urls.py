6059408bd0ad11758288f88b6fde401a9b00f908
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2024-10-23 18:16:28 -0300

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard_view, configuration_view, get_water_usage_data, get_data_collection_data
+from .views import dashboard_view, configuration_view, get_water_usage_data, get_data_collection_data, public_dashboard_view
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),
+    path('public/', public_dashboard_view, name='public_dashboard'),
     path('configuration/', configuration_view, name='configuration'),
     path('get-water-usage/', get_water_usage_data, name='get_water_usage_data'),
     path('get-data-collection/', get_data_collection_data, name='get_data_collection_data'),
